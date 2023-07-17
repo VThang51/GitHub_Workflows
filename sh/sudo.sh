df075c4ba642ba8a07932cb545e645b67d2169a1
Nguyễn Viết Thắng <118606753+VThang51@users.noreply.github.com>
2023-07-17 23:54:19 +0700

    Update sudo.sh

--- sh/sudo.sh
+++ sh/sudo.sh
@@ -204,9 +204,10 @@ if $no; then
 fi
 
 if $installrepo; then
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
-chmod a+x ~/bin/repo
-sudo ln -sf ~/bin/repo /usr/bin/repo
+    export REPO=$(mktemp /tmp/repo.XXXXXXXXX)
+    curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
+    gpg --recv-key 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
+    curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
 fi
 
 if $setuptwrp; then
